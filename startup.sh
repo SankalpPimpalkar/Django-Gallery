64d739ffc00dfc4488f335c6a89f75d948971c2e
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-27 23:39:54 +0530

    configured apprunner.yaml [BUG FIX 16]

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn gallery.wsgi:application --bind 0.0.0.0:8000
+python3 manage.py collectstatic && gunicorn gallery.wsgi:application --bind 0.0.0.0:8000
