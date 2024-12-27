b38cb579a664a91186941aa8e610da6a9709a1c5
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-27 20:30:06 +0530

    configured apprunner.yaml [BUG FIX]

--- startup.sh
+++ startup.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 gallery.wsgi
+
+# Apply database migrations
+echo "Running migrations..."
+python3 manage.py migrate
+if [ $? -ne 0 ]; then
+    echo "Error: Migration failed."
+    exit 1
+fi
+
+# Collect static files
+echo "Collecting static files..."
+python3 manage.py collectstatic --no-input
+if [ $? -ne 0 ]; then
+    echo "Error: Collecting static files failed."
+    exit 1
+fi
+
+# Start Gunicorn server
+echo "Starting Gunicorn server..."
+exec gunicorn --workers 2 gallery.wsgi:application --bind 0.0.0.0:8000
